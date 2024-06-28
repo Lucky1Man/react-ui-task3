12d87bbc4a9a3a81a43507023e0680baa234d6e1
Orest <oresttsytsyak02@gmail.com>
2024-06-28 18:23:12 +0300

    added configuration from environment variables

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,11 @@
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+
 const config = {
   // Services
-  USERS_SERVICE: 'http://localhost:1000',
+  USERS_SERVICE: BACKEND_URL,
   UI_URL_PREFIX: process.env.REACT_APP_UI_URL_PREFIX || '',
-  EXECUTION_FACTS_SERVICE: 'http://localhost:1000',
-  GOOGLE_OAUTH_LINK: 'http://localhost:1000/oauth/authenticate',
+  EXECUTION_FACTS_SERVICE: BACKEND_URL,
+  GOOGLE_OAUTH_LINK: `${BACKEND_URL}/oauth/authenticate`,
 };
 
 export default config;
